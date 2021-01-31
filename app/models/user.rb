d10d97b208b676c83ee89fddbb4d1cc21ebe4cbd
manaw0 <71313417+manaw0@users.noreply.github.com>
2021-01-31 19:18:48 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name, length: { maximum: 10 }
+    validates :birthday
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  end
 end
