dfda43043c7af2026f18b191301fcf5139b43120
manaw0 <71313417+manaw0@users.noreply.github.com>
2021-01-31 23:41:22 +0900

    itemテーブル作成

--- db/migrate/20210131143413_create_items.rb
+++ db/migrate/20210131143413_create_items.rb
@@ -1,7 +1,12 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :title,              null: false
+      t.text   :text,               null: false
+      t.integer :category_id,       null: false
+      t.integer :status_id,         null: false
+      t.integer :age_id,            null: false
+      t.references :user,           null: false, foreign_key: true
       t.timestamps
     end
   end
