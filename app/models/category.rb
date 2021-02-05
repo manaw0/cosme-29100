614db4d18fd5b603a17dd9b35a2d35c781c21586
manaw0 <71313417+manaw0@users.noreply.github.com>
2021-02-05 19:18:16 +0900

    ActiveHashカテゴリー変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'プチプラ' },
-    { id: 3, name: 'デパコス' }
+    { id: 2, name: '乾燥' },
+    { id: 3, name: 'テカリ' },
+    { id: 4, name: 'ニキビ' },
+    { id: 5, name: '毛穴' }
   ]
 
   include ActiveHash::Associations
